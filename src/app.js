acd4cb3f5af4ef047be77561f9572d9f2454fbc4
Svilen Stoev <svilen.d.stoev@abv.bg>
2022-03-03 16:52:03 +0200

    Implemented register view

--- src/app.js
+++ src/app.js
@@ -4,11 +4,13 @@ import { loginPage } from './views/login.js';
 import { logout } from './api/user.js';
 import addSession from './middlewares/session.js';
 import { decorateContext, updateUserNav } from './middlewares/render.js';
+import { registerPage } from './views/register.js';
 
 page(addSession);
 page(decorateContext);
 page('/', homePage);
 page('/login', loginPage);
+page('/register', registerPage);
 
 page.start();
 
---
+++ src/views/register.js
@@ -0,0 +1,57 @@
+import { register } from '../api/user.js';
+import { html, until } from '../lib.js';
+import { createSubmitHandler } from '../util.js';
+import { field } from './common.js';
+
+const registerTemplate = (onSubmit, errors, data) => html`
+<form @submit=${onSubmit}>
+    <h2>Register</h2>
+    <div class="on-dark">
+        ${field({ label: 'Username', name: 'username', value: data.username, placeholder: 'John93', error: errors.username })}
+    </div>
+    <div class="on-dark">
+        ${field({ label: 'Email', name: 'email', type: 'email', value: data.email, placeholder: 'John93@gmail.com', error: errors.email })}
+    </div>
+    <div class="on-dark">
+        ${field({ label: 'Password', name: 'password', type: 'password', placeholder: '*********', error: errors.password})}
+    </div>
+    <div class="on-dark">
+        ${field({ label: 'Repeat Password', name: 'repeatPass', type: 'password', placeholder: '*********', error: errors.repeatPass})}
+    </div>
+    <button class="btn" type="submit">Register</button>
+</form>`;
+
+export function registerPage(ctx) {
+    update();
+
+    function update(errors = {}, data = {}) {
+        ctx.render(registerTemplate(createSubmitHandler(onSubmit, 'username', 'password', 'repeatPass', 'email'), errors, data));
+    }
+
+    async function onSubmit({ username, password, repeatPass, email }, event) {
+        try {
+            if (username == '' || password == '' || repeatPass == '') {
+                throw {
+                    username: username == '' ? 'Username is required!' : '',
+                    email: email == '' ? 'Email is required!' : '',
+                    password: password == '' ? 'Password is required!' : '',
+                    repeatPass: repeatPass == '' ? 'Repeat password is required!' : ''
+                };
+            }
+
+            SlickLoader.enable();
+
+            await register(username, password, email);
+
+            SlickLoader.disable();
+            event.target.reset();
+            ctx.updateSession();
+            ctx.updateUserNav();
+            ctx.page.redirect('/');
+        } catch (err) {
+            SlickLoader.disable();
+
+            update(err, { username, email });
+        }
+    }
+}
